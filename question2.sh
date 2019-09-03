4a42b7dd42b55815469c0a83acf093658464eb23
RaniHirave <rani.t.hirave@gmail.com>
2019-09-03 12:21:13 +0530

    script commited

--- question2.sh
+++ question2.sh
@@ -1,32 +1,13 @@
 #!/usr/bin/expect
 
-## Access CLI
-set loginUser "admin"
-set loginPassword "password"
-set mgmtServerAddress manager_host
-
 ## Expect Parameters
 set timeout 20
 set successMsg "Status: Success"
 set failureMsg "Status: Failure"
 
-spawn ssh -l $loginUser $mgmtServerAddress -p 10000
+apt-get install nmap
 expect_after eof {exit 0}
 
 set timeout 10
 
-##interact with SSH
-##expect "yes/no" {send "yes\r"}
-expect "password:" {send "$loginPassword\r"}
-puts "\n## Starting Generated OVMCLI Script... ##\n"
-set timeout 600
-
-expect "OVM> "
-send "set OutputMode=Verbose\r"
-expect $successMsg {} \
-    timeout { puts "\n\nTest Failure: \n\r"; exit} 
-
-expect "OVM> "
-  send  "list Server\r"
-  expect $successMsg {} \
-   timeout { puts "\n\nScript Failure: \n\r"; exit}
+apt install nfs-common
